5392bdfdb471dd144ae7d937f3597585a9a7ce40
morethancoder <aalili.234.alico@gmail.com>
2023-12-02 10:49:01 +0300

    added comments

--- lua/alithedev/remap.lua
+++ lua/alithedev/remap.lua
@@ -1,43 +1,57 @@
--- here we set our keymaps
+-- Set the mapleader to a space
 vim.g.mapleader = " "
+
+-- Define a custom keymap for normal mode to execute the Ex command
 vim.keymap.set("n", "<leader><leader>", vim.cmd.Ex)
 
+-- Custom keymaps for visual mode to move selected lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Move lines down in normal mode, maintaining cursor position
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep the cursor at the same position when scrolling with <C-d> and <C-u>
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep the cursor centered when navigating with 'n' and 'N'
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
+-- Delete and yank operations using the system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
+-- Remap <C-c> in insert mode to <Esc>
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- Disable the default behavior of Q (Ex mode) in normal mode
 vim.keymap.set("n", "Q", "<nop>")
+
+-- Format the current buffer using LSP
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- Quickfix list and location list navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- Search and replace in the entire buffer, case-insensitive
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Add execute permission to the current file
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-
+-- Execute ':so' to source the current file
 vim.keymap.set("n", "<leader>so", function()
     vim.cmd("so")
 end)
 
+-- Execute ':w' to write/save the current buffer
 vim.keymap.set("n", "<leader>w", function()
     vim.cmd("w")
 end)
+
--- lua/alithedev/set.lua
+++ lua/alithedev/set.lua
@@ -1,35 +1,53 @@
 -- Enable clipboard support using xclip
 vim.cmd('set clipboard=unnamedplus')
 
+-- Set the guicursor to an empty string (no cursor changes)
 vim.opt.guicursor = ""
+
+-- Enable line numbers and relative line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Set tab-related options
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Enable smart indenting
 vim.opt.smartindent = true
 
+-- Disable line wrapping
 vim.opt.wrap = false
 
+-- Disable swap files, backups, and use a custom undodir
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Disable highlighting during search and enable incremental search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Enable true color support in the terminal
 vim.opt.termguicolors = true
 
+-- Set the number of lines to keep above and below the cursor when scrolling
 vim.opt.scrolloff = 8
+
+-- Display the sign column (for signs like LSP diagnostics)
 vim.opt.signcolumn = "yes"
+
+-- Add '@-@' to the list of characters considered as part of a word
 vim.opt.isfname:append("@-@")
 
+-- Set the time (in milliseconds) to wait for more key presses after a mapped sequence
 vim.opt.updatetime = 50
 
+-- Highlight the column at character position 115
 vim.opt.colorcolumn = "115"
 
+-- Set the mapleader to a space
 vim.g.mapleader = ' '
+
